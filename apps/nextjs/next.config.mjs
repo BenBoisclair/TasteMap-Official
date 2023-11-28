77cf5de71e5477d3a2e4d03fce8ff538066617b2
BBoisclair <ben.b.boisclair@gmail.com>
2023-11-29 02:21:23 +0700

    "Migrated to Turbo"

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,15 +1,36 @@
 // Importing env files here to validate on build
 import "./src/env.mjs";
-import "@acme/auth/env.mjs";
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: ["@acme/api", "@acme/auth", "@acme/db"],
+  transpilePackages: ["@acme/db"],
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "adlvkgocidkmifehftjz.supabase.co",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default config;
