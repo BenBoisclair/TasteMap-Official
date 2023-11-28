f5b47a3519c90c604d423f1602d81094742485c9
BBoisclair <ben.b.boisclair@gmail.com>
2023-11-29 02:21:23 +0700

    "Migrated to Turbo"

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+
+import ttnorms from "~/fonts/ttnorms";
 
 import "~/styles/globals.css";
 
 import { headers } from "next/headers";
+import { ClerkProvider } from "@clerk/nextjs";
 
-import { TRPCReactProvider } from "./providers";
+import { cn } from "~/utils/cn";
+import { Providers } from "./providers";
 
-const fontSans = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+// const fontSans = Inter({
+//   subsets: ["latin"],
+//   variable: "--font-sans",
+// });
 
 /**
  * Since we're passing `headers()` to the `TRPCReactProvider` we need to
@@ -37,12 +40,12 @@ export const metadata: Metadata = {
 
 export default function Layout(props: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={["font-sans", fontSans.variable].join(" ")}>
-        <TRPCReactProvider headers={headers()}>
-          {props.children}
-        </TRPCReactProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn(ttnorms.className, "text-neutral-800 antialiased")}>
+          <Providers headers={headers()}>{props.children}</Providers>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- tooling/tailwind/index.ts
+++ tooling/tailwind/index.ts
@@ -3,7 +3,27 @@ import type { Config } from "tailwindcss";
 export default {
   content: [""],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: { 600: "#956E00", DEFAULT: "#FFD14E" },
+        neutral: {
+          800: "#3F3F3F",
+          600: "#6F6F6F",
+          DEFAULT: "#F3F3F3",
+        },
+        gray: "#E4E3DE",
+        green: {
+          600: "#045453",
+          DEFAULT: "#33BFBE",
+        },
+        orange: { 600: "#EF4E3D", DEFAULT: "#EB5E2A" },
+        blue: "#7F9ECE",
+      },
+      fontSize: {
+        "2xs": "0.625rem",
+        "3xs": "0.5rem",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
