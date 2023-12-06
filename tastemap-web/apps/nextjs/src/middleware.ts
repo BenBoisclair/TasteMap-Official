f221a98f501d0cf0bf0d4e68a7dd065b432dc678
BBoisclair <ben.b.boisclair@gmail.com>
2023-12-06 21:37:40 +0700

    Configured Clerk Middleware to allow Webhooks to work

--- tastemap-web/apps/nextjs/src/middleware.ts
+++ tastemap-web/apps/nextjs/src/middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs/server";
 export default authMiddleware({
   publicRoutes: [
     "/(.*)",
-    "/api/webhooks(.*)",
+    "/api/webhooks/users",
     "/api/market(.*)",
     "/api/vendor(.*)",
   ],
