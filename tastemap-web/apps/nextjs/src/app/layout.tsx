72b32e28f0ada5437a7a37c37598b9fe0997835f
BBoisclair <ben.b.boisclair@gmail.com>
2023-12-09 03:24:58 +0700

    Added Vendor Recommendations Page in Markets

--- tastemap-web/apps/nextjs/src/app/layout.tsx
+++ tastemap-web/apps/nextjs/src/app/layout.tsx
@@ -5,7 +5,6 @@ import ttnorms from "~/fonts/ttnorms";
 
 import "~/styles/globals.css";
 
-import Head from "next/head";
 import { headers } from "next/headers";
 import Script from "next/script";
 import { ClerkProvider } from "@clerk/nextjs";
