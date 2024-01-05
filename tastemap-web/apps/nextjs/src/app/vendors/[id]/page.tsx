04aaaf6111acd2f9a91fb1b4e6d6accad5f8fb00
BBoisclair <ben.b.boisclair@gmail.com>
2024-01-05 23:16:25 +0700

    - Made Vendor Page Server Side Render

--- tastemap-web/apps/nextjs/src/app/vendors/[id]/page.tsx
+++ tastemap-web/apps/nextjs/src/app/vendors/[id]/page.tsx
@@ -1,69 +1,21 @@
-"use client";
-
-import { useState } from "react";
-// import { useRouter } from "next/navigation";
-import { useQuery } from "@tanstack/react-query";
-import { useInView } from "react-intersection-observer";
-
-import EventElements from "~/components/event-elements";
-import LoadingPage from "~/components/pages/loading-page";
-import RatingAndReviewSection from "~/components/sections/RatingsAndReviews/rating-and-reviews-section";
-import Tabs from "~/components/tabs";
-import VendorHeader from "~/components/vendor/vendor-header";
-import VendorInfoPage from "~/components/vendor/vendor-info-page";
-import type { Vendor } from "~/types/types";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { Vendor } from "~/types/types";
 import fetchAt from "~/utils/fetchAt";
+import { queryClient } from "~/utils/queryClient";
+import VendorView from "./vendor-view";
 
-export default function Vendor({ params }: { params: { id: string } }) {
-  const vendorId = params.id;
-
-  const { data: vendor, status } = useQuery({
+export default async function VendorPage({
+  params: { id: vendorId },
+}: {
+  params: { id: string };
+}) {
+  await queryClient.prefetchQuery({
     queryKey: ["vendor", vendorId],
     queryFn: () => fetchAt<Vendor>(`/api/vendors/${vendorId}`),
   });
-
-  const [activeTab, setActiveTab] = useState<string>("Info");
-
-  const { ref: headerRef, inView } = useInView({
-    threshold: 0,
-    rootMargin: "-50px",
-  });
-
-  const handleTabSelect = (tabName: string) => {
-    setActiveTab(tabName);
-    if (!inView) {
-      window?.scrollTo(0, 380);
-    }
-  };
-
-  if (status === "pending") {
-    return <LoadingPage />;
-  }
-
-  if (status === "error") {
-    return <div>Error</div>;
-  }
-
   return (
-    <div className="relative">
-      <EventElements />
-
-      <VendorHeader vendor={vendor} inView={inView} headerRef={headerRef} />
-      <Tabs
-        activeTab={activeTab}
-        handleTabSelect={handleTabSelect}
-        inView={inView}
-        tabs={["Info", "Reviews"]}
-      />
-      {activeTab === "Info" && <VendorInfoPage vendor={vendor} />}
-      {activeTab === "Reviews" && (
-        <RatingAndReviewSection
-          id={vendorId}
-          type="vendor"
-          name={vendor.name}
-          imageUrl={vendor.bannerUrl ?? ""}
-        />
-      )}
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <VendorView params={{ id: vendorId }} />
+    </HydrationBoundary>
   );
 }
