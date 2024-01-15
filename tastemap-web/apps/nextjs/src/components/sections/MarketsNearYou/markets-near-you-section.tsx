445b607a32fe21d494b8d6e7d2f9081e72eec2e2
BBoisclair <ben.b.boisclair@gmail.com>
2024-01-15 21:01:16 +0700

    - Bug Fix

--- tastemap-web/apps/nextjs/src/components/sections/MarketsNearYou/markets-near-you-section.tsx
+++ tastemap-web/apps/nextjs/src/components/sections/MarketsNearYou/markets-near-you-section.tsx
@@ -1,8 +1,6 @@
 import { MarketCard } from "../../market/market-card";
 import { getMarkets } from "~/app/_actions/actions";
 
-export const dynamic = "force-dynamic";
-
 export async function MarketsNearYouSection() {
   const markets = await getMarkets();
 
